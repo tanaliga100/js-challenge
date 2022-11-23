c64f4998f521c31bdb7e7895f89bbb4b2006b6f9
tanaliga100 <jordantanaliga@gmail.com>
2022-11-23 16:12:19 +0800

    add-challenge

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("this is the main-branch");
+export const Test = () => {
+  function longestWord(params) {
+    let str = params.split(" ");
+    let size = 0;
+    let longest = [""];
+    for (let i = 0; i < str.length; i++) {
+      const element = str[i];
+      if (element.length >= size) {
+        size = element.length;
+        if (element.length > longest[longest.length - 1].length) {
+          longest = [];
+          longest.push(element);
+        } else {
+          longest = [...longest, element];
+        }
+      }
+    }
+    return longest;
+  }
+
+  const res = longestWord("The piano sounds good");
+  console.warn("RESULTS:", res);
+};
