71cd2024ec226efd13800ab252ad9fdfa7f2745e
tanaliga100 <jordantanaliga@gmail.com>
2022-11-23 16:27:32 +0800

    add-sandbox

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("this is the main-branch");
+export const Test = () => {
+  function testFn(params) {
+    return params;
+  }
+
+  const res = testFn("THIS IS A TEST ");
+  console.warn("RESULTS:", res);
+};
