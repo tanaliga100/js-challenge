5ca822a17763686701e496a08745d00e39189c13
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 15:35:54 +0800

    finale

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const Test = () => {
   function testFn(params) {
-    return params;
+    let tempArr = params.sort((x, y) => {
+      return x - y;
+    });
+    let largest = tempArr.pop();
+    let number = 0;
+    tempArr.reduce((acc, curr) => {
+      number = number + curr;
+      return acc;
+    }, 0);
+    return number === largest ? "Correct Summation" : "Incorrect Summation";
   }
 
-  const res = testFn("THIS IS A TEST ");
+  const res = testFn([1, 11, 3, 5, 101]);
+  const res2 = testFn([1, 2, 3, 6]);
+
   console.warn("RESULTS:", res);
+  console.warn("RESULTS:", res2);
 };
 
 Test();
