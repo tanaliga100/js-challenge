7c8269ca03a5688cafee1e0b3930cef22adf86c4
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 10:25:42 +0800

    finale

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const Test = () => {
-  function testFn(params) {
-    return params;
-  }
+  //LONG - METHOD
+  // function capitalizeLetter(params) {
+  //   const str = params.split(" ").map((letter) => {
+  //     let first = letter.charAt(0).toUpperCase();
+  //     let rest = letter.slice(1);
+  //     return `${first}${rest}`;
+  //   });
 
-  const res = testFn("THIS IS A TEST ");
+  //   return str.join(" ");
+  // }
+  // SHORT METHOD
+  const capitalizeLetter = (params) => {
+    const str = params
+      .split(" ")
+      .map((letter) => letter.charAt(0).toUpperCase() + letter.slice(1));
+    return str.join(" ");
+  };
+
+  const res = capitalizeLetter("this is a test ");
   console.warn("RESULTS:", res);
 };
 
