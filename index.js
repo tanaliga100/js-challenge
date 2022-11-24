d1c702193e8f3f5f466c93dee661b2d3df40807b
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 18:11:01 +0800

    initial

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const Test = () => {
-  const string = "Javascript is the best programming language";
-
   function testFn(params) {
-    let str = params.split(" ");
-    str = str.map((item) => {
-      let tempItem = item.toUpperCase().split("");
-      return tempItem.reduce(
-        (acc, curr) => {
-          acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
-          if (acc[curr] > acc.max) {
-            acc.max = acc[curr];
-          }
-          return acc;
-        },
-        { max: 1, word: item.toUpperCase() }
-      );
-    });
-    let amount = 1;
-    let word = "";
-    for (let item of str) {
-      if (item.max > amount) {
-        amount = item.max;
-        word = item.word;
-      }
-    }
-    if (amount > 1) {
-      return word;
-    }
-    return -1;
+    return params;
   }
-  const res = testFn(string);
-  console.log("RESULTS:", res);
+
+  console.log("Results", testFn([1, -2, 5, 7, 8]));
+  console.log("Results", testFn([1, -7, 8]));
+  console.log("Results", testFn([1, 2, -5, 7, 8]));
 };
 
 Test();
