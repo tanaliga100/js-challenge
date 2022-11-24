dfa70b9c16b8d799fdaf05dd68ec7a19fe4ed458
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 16:48:40 +0800

    finale

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const Test = () => {
   function testFn(params) {
-    return params;
-  }
+    // LONG METHOD
+    // let tempString = new Set();
+    // for (let letter in params) {
+    //   if (tempString.has(letter)) {
+    //     return false;
+    //   }
+    //   tempString.add(params[letter]);
+    // }
+    // return tempString;
+    // SHORT METHOD
 
-  const res = testFn("THIS IS A TEST ");
+    let tempString = new Set();
+    for (let letter in params) {
+      if (tempString.has(letter)) {
+        return "already exists";
+      }
+      tempString.add(params[letter]);
+    }
+    return tempString;
+  }
+  const res = testFn([1, 2, 3, 4, 5, 5]);
   console.warn("RESULTS:", res);
 };
 
