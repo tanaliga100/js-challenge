206aaf8e3e126eedaabea069fd9d81799a2159f5
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 17:01:55 +0800

    finale

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const Test = () => {
-  function testFn(params) {
-    // LONG METHOD
-    // let tempString = new Set();
-    // for (let letter in params) {
-    //   if (tempString.has(letter)) {
-    //     return false;
-    //   }
-    //   tempString.add(params[letter]);
-    // }
-    // return tempString;
-    // SHORT METHOD
+  const products = [
+    { title: "Iphone 8", company: "Apple" },
+    { title: "Samsung A20", company: "Samsung" },
+    { title: "Galaxy S5", company: "Samsung" },
+    { title: "Iphone 10", company: "Apple" },
+    { title: "Iphone X", company: "Apple" },
+    { title: "ThinkPad", company: "Lenovo" },
+    { title: "Ideapad 3", company: "Lenovo" },
+  ];
 
-    let tempString = new Set();
-    for (let letter in params) {
-      if (tempString.has(letter)) {
-        return "already exists";
-      }
-      tempString.add(params[letter]);
-    }
-    return tempString;
+  function testFn(params) {
+    // USING MAP
+    let tempArray = params.map((item) => item.company);
+    return [...new Set(tempArray)];
+    // USING REDUCE
+    return [
+      ...params.reduce((acc, curr) => {
+        acc.add(curr.company);
+        return acc;
+      }, new Set()),
+    ];
   }
-  const res = testFn([1, 2, 3, 4, 5, 5]);
+  const res = testFn(products);
   console.warn("RESULTS:", res);
 };
 
