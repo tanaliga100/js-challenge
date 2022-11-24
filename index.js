4ba36503f98d14bbb118d29e3131bb3c746f8940
tanaliga100 <jordantanaliga@gmail.com>
2022-11-24 14:58:31 +0800

    finale

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const Test = () => {
-  function mathSequence(numbers) {
-    const arr = numbers.map(Math.abs);
-    let arith = new Set();
-    let geo = new Set();
-    for (let i = 1; i < arr.length; i++) {
-      // note ! arr[i] = is the first index of the array
-      // note ! i subtract arr[i] to the last index of the array which is index zero
-      const num1 = arr[i] - arr[i - 1];
-      arith.add(num1);
-      const num2 = arr[i] / arr[i - 1];
-      geo.add(num2);
+  const uniqueValues = (params) => {
+    // USE CASE IN THE ARRAY METHOD
+    // const str = params.split("");
+    // let uniqueValues = [];
+    // console.log(Array.isArray(str) ? "oo" : "hindi");
+    // for (let letter of str) {
+    //   if (uniqueValues.indexOf(letter) === 0)
+    //     return `Oops! There are duplicate values`;
+    //   uniqueValues.push(letter);
+    // }
+    // return uniqueValues;
+    // USE IN THE OBJECT
+    // const str = params.split("");
+    // let uniqueValues = {};
+    // let val = 1;
+    // for (let letter of str) {
+    //   if (uniqueValues[letter]) {
+    //     return false;
+    //   }
+    //   uniqueValues[letter] = val++;
+    // }
+    // return uniqueValues;
+    // USING THE INDEX || NORMAL FOR LOOP
+    const str = params.split("");
+    let uniqueValues = [];
+    for (let i = 0; i < str.length; i++) {
+      let curr = str[i];
+      let index = i;
+      if (str.lastIndexOf(curr) !== index) {
+        return "duplicate values are : " + str[i].split(" ");
+      } else {
+        return [...uniqueValues, str];
+      }
     }
-    if (arith.size === 1) {
-      return `Arithmetic`;
-    } else if (geo.size === 1) {
-      return `Geometric`;
-    } else {
-      return -1;
-    }
-  }
+    return;
+  };
+  // const instruments = ["guitar", "piano", "brass", "piano"];
+  console.log("FIRST_RESULTS_:", uniqueValues("cbcdefg"));
+  console.log("SECOND_RESULTS_:", uniqueValues("abcdefg"));
 
-  console.log("RESULTS_:", mathSequence([2, -4, 6, 8, 10]));
-  console.log("RESULTS_:", mathSequence([3, 9, -27]));
-  console.log("RESULTS_:", mathSequence([2, 8, 23, 76]));
+  // console.log("RESULTS_:", uniqueValues(person));
+  // console.log("RESULTS_:", uniqueValues(instruments));
 };
 
 Test();
