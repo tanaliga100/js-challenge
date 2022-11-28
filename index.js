84c07b09d22437f8ff64682c16b56b5f6f8a700f
tanaliga100 <jordantanaliga@gmail.com>
2022-11-28 13:01:24 +0800

    finale

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const Test = () => {
   function testFn(params) {
-    return params;
+    let withOutVowels = [];
+    const words = params.split(" ").join("").split("");
+    for (let i = 0; i < words.length; i++) {
+      if (
+        words[i].toLowerCase() === "a" ||
+        words[i].toLowerCase() === "e" ||
+        words[i].toLowerCase() === "i" ||
+        words[i].toLowerCase() === "o" ||
+        words[i].toLowerCase() === "u"
+      ) {
+        continue;
+      }
+      withOutVowels.push(words[i]);
+    }
+    return withOutVowels.toString();
   }
-
   const res = testFn("THIS IS A TEST ");
-  console.warn("RESULTS:", res);
+  console.log("RESULTS:", res);
 };
-
 Test();
